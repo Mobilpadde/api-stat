c142c46286c2ede3b3d6fa02317c59cc9b7ce7eb
Mads Bram Cordes <Mobilpadde@gmail.com>
2021-09-28 00:50:00 +0200

    Add vercel build

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,13 @@
 import { windi } from 'svelte-windicss-preprocess';
+import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: [windi({})],
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: vercel()
 	}
 };
 
